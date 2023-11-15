00ecbb85ef5e3f60b8e2db22f0b6ffe0aed28b0f
Lang-Haruka <150527136+Lang-Haruka@users.noreply.github.com>
2023-11-15 10:27:06 +0900

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,24 @@
+// Scroll
+$(function(){
+  $('a[href^="#"]').click(function() {
+    let speed = 400;
+    let type = 'swing';
+    let href= $(this).attr("href");
+    let target = $(href == "#index" ? 'html' : href);
+    let position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, type);
+    return false;
+  });
+});
+
+// Accordion
+jQuery(function ($) {
+  $(".answer").css("display", "none");
+  $(".answer1").css("display", "none");
+  $(".question").click(function () {
+    $(".question").not(this).removeClass("open");
+    $(".question").not(this).next().slideUp(300);
+    $(this).toggleClass("open");
+    $(this).next().slideToggle(300);
+  });
+});
